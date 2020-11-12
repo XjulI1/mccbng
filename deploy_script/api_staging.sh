d88c755508a0b4ef9fd28810ee5444d619ba7645
Xavier JULIEN <xjulien@molotov.tv>
2020-11-12 08:12:45 +0100

    Add build docker api staging

--- deploy_script/api_staging.sh
+++ deploy_script/api_staging.sh
@@ -1,2 +1,2 @@
 docker pull xjuli1/mccbng_api:staging
-docker run -d --rm -p 3003:3001 --mount type=bind,source=/home/ubuntu/mccbng/datasources-staging.json,target=/usr/src/app/server/datasources.json xjuli1/mccbng_api:staging
+docker run -d --rm -p 3003:3001 --env NODE_ENV=development --mount type=bind,source=/home/ubuntu/mccbng/datasources-staging.json,target=/usr/src/app/server/datasources.json xjuli1/mccbng_api:staging
