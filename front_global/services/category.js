b7bcb751381e3259554c48ca08feb34d122dfccd
Xavier JULIEN <xjulien@molotov.tv>
2021-01-01 10:57:59 +0100

    Change front API calls with new contract

--- front_global/services/category.js
+++ front_global/services/category.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const fetchCategoryList = (IDuser, userToken, APIURL) => {
   const filter = { where: { or: [{ IDuser }, { IDuser: 0 }] }, order: 'Nom ASC' }
 
-  return axios.get(APIURL + '/api/Categories', {
+  return axios.get(APIURL + '/api/categories', {
     params: {
       access_token: userToken,
       filter
--- front_global/services/stats.js
+++ front_global/services/stats.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 export const fetchEvolutionSolde = (userID, userToken, APIURL) => {
-  return axios.get(APIURL + '/api/stats/evolutionSolde', {
+  return axios.get(APIURL + '/api/stats/evolutionSolde/' + userID, {
     params: {
-      access_token: userToken,
-      userID
+      access_token: userToken
     }
   }).then((response) => {
     return response.data
