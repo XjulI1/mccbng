6a0829f5edef0168d0a23a2bb578c61e7aa13276
Xavier JULIEN <xjulien@molotov.tv>
2022-08-30 10:37:22 +0200

    Feat: remove react part

--- front_react/src/store/Operation/action.js
+++
@@ -1,5 +0,0 @@
-export const SAVE_CURRENT_ACCOUNT_OPERATIONS = 'SAVE_CURRENT_ACCOUNT_OPERATIONS'
-
-export function saveCurrentAccountOperations (operationsList) {
-  return { type: SAVE_CURRENT_ACCOUNT_OPERATIONS, operationsList }
-}
