40a29e7a8240250aa776c6c73bd1cc2e4ee34948
Xavier J <xjulien@invivo-group.com>
2023-04-04 14:48:52 +0200

    feat: new bank logo

--- front_vue/src/main.js
+++ front_vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import Vue2TouchEvents from 'vue2-touch-events'
+// import Vue2TouchEvents from 'vue2-touch-events'
 import fontawesome from './plugins/fontawesome'
 
 import './registerServiceWorker'
@@ -12,7 +12,7 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
-app.use(Vue2TouchEvents)
+// app.use(Vue2TouchEvents)
 
 app.component('FontAwesomeIcon', fontawesome)
 
