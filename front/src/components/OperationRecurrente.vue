609112c58f09eaeee0902aa4ce5938776cb16bb9
Xavier J <xjulien@invivo-group.com>
2025-06-27 15:37:19 +0200

    feat: add account name in recur ops

--- front/src/components/OperationRecurrente.vue
+++ front/src/components/OperationRecurrente.vue
@@ -1,58 +1,63 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-9">
+      <div class="col-6">
         <label>
           {{ operation.NomOpRecu }}
-          <br>
+          <br />
           {{ dateOperation }}
         </label>
       </div>
-      <div
-        class="col-3 center-text"
-        :class="css.montant"
-      >
+      <div class="col-2 center-text" :class="css.montant">
         <Currency :amount="operation.MontantOpRecu" />
       </div>
+      <div class="col-4">{{ accountName }}</div>
     </div>
   </div>
 </template>
 
 <script>
-  import Currency from './Currency'
+import Currency from "./Currency";
 
-  export default {
-    name: 'OperationRecurrente',
-    components: { Currency },
-    props: {
-      operation: {
-        type: Object,
-        default: () => {
-        }
-      }
+export default {
+  name: "OperationRecurrente",
+  components: { Currency },
+  props: {
+    operation: {
+      type: Object,
+      default: () => {},
     },
+  },
 
-    data () {
-      return {
-        dateOperation: new Date(this.operation.DernierDateOpRecu).toLocaleDateString(),
-        css: {
-          montant: this.operation.MontantOpRecu > 0 ? 'montantIn' : 'montantOut'
-        }
-      }
-    }
-  }
+  data() {
+    const store = this.$store;
+
+    return {
+      dateOperation: new Date(
+        this.operation.DernierDateOpRecu
+      ).toLocaleDateString(),
+      css: {
+        montant: this.operation.MontantOpRecu > 0 ? "montantIn" : "montantOut",
+      },
+      accountName:
+        store.state.compte.accountList.find(
+          (account) => account.IDcompte === this.operation.IDcompte
+        )?.NomCompte || "Unknown Account",
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .center-text {
-    text-align: center;
-  }
+.center-text {
+  text-align: center;
+}
 
-  .montantIn {
-    color: green;
-  }
+.montantIn {
+  color: green;
+}
 
-  .montantOut {
-    color: red
-  }
+.montantOut {
+  color: red;
+}
 </style>
